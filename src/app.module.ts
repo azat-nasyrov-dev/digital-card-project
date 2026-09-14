586af80a1a000e3cea46e78a6664e8859bf5008c
Azat Nasyrov <azat.nasyrov31@gmail.com>
2026-09-14 20:47:40 +0300

    clean up a project after generating cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({})
 export class AppModule {}
